1d1a059a94c275da2a63b301c289cc62fc5600fb
MnqobiSB <amandlamm1@gmail.com>
2020-09-12 18:44:01 +0200

    first deploy commit v1

--- cloudinary/index.js
+++ cloudinary/index.js
@@ -2,7 +2,7 @@ const crypto = require('crypto');
 const cloudinary = require('cloudinary');
 cloudinary.config({
 	cloud_name: 'suburban-digi-hustle',
-	api_key: '625154945747452',
+	api_key: process.env.CLOUDINARY_APIKEY,
 	api_secret: process.env.CLOUDINARY_SECRET
 });
 const cloudinaryStorage = require('multer-storage-cloudinary');
